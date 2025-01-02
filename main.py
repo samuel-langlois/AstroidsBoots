b73ff05e8a804ed815df9f6255d28b021121fc2b
samuel-langlois <samuel.j.langlois@gmail.com>
2025-01-02 16:06:51 -0700

    v0.3: added a pygame clock and tick counter

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ def main ():
     print(f"Screen height: {SCREEN_HEIGHT}")
     # remove above later?
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
-        screen.fill((0,0,0), rect=None, special_flags=0)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill((0,0,0), rect=None, special_flags=0)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
